b62684c3aa26a04352c71e2dfaec110476892e8f
Mirka <mjackuliakova@student.umb.sk>
2022-06-21 22:37:02 +0200

    vytvorené okno upravovania

--- src/swing/DetailPolozky.java
+++ src/swing/DetailPolozky.java
@@ -1,4 +1,25 @@
 package swing;
 
-public class DetailPolozky {
+import model.Polozka;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DetailPolozky extends JDialog {
+    public DetailPolozky(TelZoznam zoznam, Moznosti moznosti, Polozka polozka){
+        super(zoznam, true);
+        switch(moznosti) {
+            case PRIDAT:
+                setTitle("Pridať položku");
+                break;
+            case UPRAVIT:
+                setTitle("Upraviť položku");
+                break;
+            case ODSTRANIT:
+                setTitle("Odstrániť položku");
+                break;
+        }
+        Container okno = getContentPane();
+        okno.setLayout(new BoxLayout(okno, BoxLayout.PAGE_AXIS));
+    }
 }
