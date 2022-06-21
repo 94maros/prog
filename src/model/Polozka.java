1708a78344fae32ba7f5949e62082e086b2e95ca
Maros <mzsilinszky@student.umb.sk>
2022-06-21 18:23:31 +0200

    struktura zoznam

---
+++ src/model/Polozka.java
@@ -0,0 +1,53 @@
+package model;
+public class Polozka {
+    private String meno;
+    private String priezvisko;
+    private String adresa;
+    private String mesto;
+    private String stat;
+    private String telC;
+
+    public void setMeno() {
+        this.meno = meno;
+    }
+    public String getMeno() {
+        return meno;
+    }
+
+    public void setPriezvisko() {
+        this.priezvisko = priezvisko;
+    }
+    public String getPriezvisko() {
+        return priezvisko;
+    }
+
+    public void setAdresa() {
+        this.adresa = adresa;
+    }
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setMesto() {
+        this.mesto = mesto;
+    }
+    public String getMesto() {
+        return mesto;
+    }
+
+    public void setStat() {
+        this.stat = stat;
+    }
+    public String getStat() {
+        return stat;
+    }
+
+    public void setTelC() {
+        this.telC = telC;
+    }
+    public String getTelC() {
+        return telC;
+    }
+
+
+}
