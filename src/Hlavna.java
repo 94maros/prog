08ef20a615810ec37e0a7bfb82411a2aeca9e691
Mirka <mjackuliakova@student.umb.sk>
2022-06-21 20:29:38 +0200

    .

--- src/Hlavna.java
+++ src/Hlavna.java
@@ -2,7 +2,7 @@ import swing.TelZoznam;
 
 import javax.swing.*;
 
-public class Hlavna {
+public class Hlavna{
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
