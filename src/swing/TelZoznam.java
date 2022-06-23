167664e220dd8bc549b24635bb1edb0e54d08e2e
Maros <mzsilinszky@student.umb.sk>
2022-06-23 16:55:12 +0200

    pokus upravit polhou - neuspesny
    hladanie funkcne cez keylistener

--- src/swing/TelZoznam.java
+++ src/swing/TelZoznam.java
@@ -4,14 +4,18 @@ import model.Polozka;
 import swing.Podrobnosti;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.LinkedList;
 import java.util.List;
 
 public class TelZoznam extends JFrame {
-
+    private JTextField hladane;
     private List<Polozka> polozky = new LinkedList<Polozka>();
     private DefaultListModel zoznamModel;
     public TelZoznam(){
@@ -20,8 +24,15 @@ public class TelZoznam extends JFrame {
         //novy kontajner
         Container okno = getContentPane();
         okno.setLayout(new BorderLayout());
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - okno.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - okno.getHeight()) / 2);
+        okno.setLocation(x, y);
+
+
         //hladanie
-        {//zoznam - vytvorenie panela v okne
+        //zoznam - vytvorenie panela v okne
         JPanel list = new JPanel();
         list.setLayout(new FlowLayout());
         okno.add(list, BorderLayout.PAGE_START);
@@ -29,14 +40,37 @@ public class TelZoznam extends JFrame {
         JLabel hladaj = new JLabel("Zadaj hľadané meno:");
         list.add(hladaj);
         //zadat meno
-        JTextField hladane = new JTextField(20);
-        list.add(hladane);}
-
-        //zoznam
+        hladane = new JTextField(20);
+        list.add(hladane);
         zoznamModel = new DefaultListModel();
         JList zoznam = new JList(zoznamModel);
         JScrollPane posuvnik = new JScrollPane(zoznam);
         okno.add(posuvnik, BorderLayout.CENTER);
+
+        //
+        hladane.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                zoznam.clearSelection();
+                String slovo = hladane.getText().trim();
+                if (slovo.length()!=0){
+                    for(int i=0;i<polozky.size();i++) {
+                        Polozka pol = polozky.get(i);
+                        if(pol.getMeno().indexOf(slovo)!=-1||pol.getPriezvisko().indexOf(slovo)!=1){
+                            zoznam.setSelectedIndex(i);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+
+
+
+        //
+        //zoznam
+
 
         JPanel tlacidla = new JPanel();
         tlacidla.setLayout(new FlowLayout());
@@ -78,31 +112,22 @@ public class TelZoznam extends JFrame {
 
         JButton odstranit = new JButton("Vymaz!");
         int pol = zoznam.getSelectedIndex();
-        //neviditelne pred oznacenim prvku
-        /*if(!zoznam.isSelectedIndex(pol)){
-            odstranit.setEnabled(false);
-            pack();
-        }*/
         odstranit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int pol = zoznam.getSelectedIndex();
-                //viditelne po oznaceni prvku
-                /*if(zoznam.isSelectedIndex(pol)){
-                    odstranit.setEnabled(true);
-                    pack();
-                }*/
+                if (pol!=1) {
+                    Polozka polozka = polozky.get(pol);
+                    DetailPolozky det = new DetailPolozky(TelZoznam.this, Moznosti.ODSTRANIT, polozka);
 
-                Polozka polozka = polozky.get(pol);
-                DetailPolozky det = new DetailPolozky(TelZoznam.this, Moznosti.ODSTRANIT, polozka);
-                //det.setVisible(true);
-
-                if(det.Zrusit()==false){
-                    zoznamModel.removeElementAt(pol);
-                    pack();
+                    if (det.Zrusit() == false) {
+                        zoznamModel.removeElementAt(pol);
+                        pack();
+                    }
                 }
             }
         });
+
 
         tlacidla.add(odstranit);
 
@@ -115,9 +140,7 @@ public class TelZoznam extends JFrame {
                 Polozka polozka = polozky.get(pol);
                 Podrobnosti det = new Podrobnosti(TelZoznam.this, Moznosti.DETAIL, polozka);
                 det.setVisible(true);
-                if (!det.Zrusit()){
-                    //zoznamModel.addElement(polozka.getMeno()+" "+polozka.getPriezvisko());
-                }
+
             }
         });
         tlacidla.add(detail);
@@ -134,4 +157,5 @@ public class TelZoznam extends JFrame {
 
                 pack();
     }
+
 }
